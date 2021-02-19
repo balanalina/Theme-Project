ba251bd75c66e6a8c4dafe6e08e33fcedf342e9d
Balan Alina <alinabalan16.ab@gmail.com>
2021-02-19 11:06:24 +0200

    add some features

---
+++ Botanophobia/Assets/Time_Limit.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Time_Limit : MonoBehaviour
+{
+    public float delay;
+    public string NewLevel;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (delay > 0)
+        {
+            delay -= Time.deltaTime;
+        }
+        else
+        {
+            Debug.Log("intra");
+            SceneManager.LoadScene(NewLevel);
+        }
+    }
+}
