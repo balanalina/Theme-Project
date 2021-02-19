a0c6df04e6b52f64bf6e5a1b4818358a87081921
Balan Alina <alinabalan16.ab@gmail.com>
2021-02-19 10:06:32 +0200

    add project

---
+++ Botanophobia/Assets/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField] float speed;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+        Vector3 moveBy = transform.right * x + transform.forward * z;
+        rb.MovePosition(transform.position + moveBy.normalized * speed * Time.deltaTime);
+    }
+    
+}
